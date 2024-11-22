687abc5222b94c16fb6f902712583a2357cc3a4f
jmathews10 <joma6354@colorado.edu>
2024-11-22 13:23:57 -0700

    wav reconstruction is doing ok

--- tests.py
+++ tests.py
@@ -4,23 +4,27 @@ import numpy as np
 import scipy.linalg as linalg
 from gen_rank_def import genRankDef
 from Rank_deficient import *
-from wav_to_spectrogram import wav_to_spectrogram
+from wav_to_spectrogram import *
 
-no_log, A = wav_to_spectrogram("test_file.wav")
+
+no_log, A, samples = wav_to_spectrogram("test_file.wav")
 
 # print(A[:5, :5])
 
-Qnew, Rnew, P = Parker_Compression(no_log, e=0.1)
+# Qnew, Rnew, P = Parker_Compression(no_log, e=0.1)
 
-print(Qnew.shape)
-print(Rnew.shape)
+# # print(Qnew.shape)
+# # print(Rnew.shape)
 
-Anew = reconstruct(Qnew, Rnew, P)
+# Anew = reconstruct(Qnew, Rnew, P)
 
 # print(Anew[:5, :5])
 
-diff = A - Anew
+# diff = A - Anew
 
-norm = linalg.norm(diff)
+# norm = linalg.norm(diff)
 
 # print(norm)
+
+# spectrogram_to_wav(no_log)
+spectrogram_to_wav_griffinlim(no_log, samples)
--- wav_to_spectrogram.py
+++ wav_to_spectrogram.py
@@ -1,7 +1,10 @@
 import numpy as np
 from scipy.io import wavfile
 from scipy.signal import spectrogram
-import matplotlib.pyplot as plt
+from scipy.signal import istft
+from scipy.io.wavfile import write
+from librosa import griffinlim
+import os
 
 
 def wav_to_spectrogram(filename):
@@ -11,10 +14,24 @@ def wav_to_spectrogram(filename):
     if data.ndim == 2:
         data = np.mean(data, axis=1)
 
-        frequencies, times, Sxx = spectrogram(data, samples, nperseg=1024)
+        frequencies, times, Sxx = spectrogram(data, samples, nperseg=1024, noverlap=512)
 
         Sxx_log = 10 * np.log10(Sxx + 1e-10)
 
         Sxx_log -= np.max(Sxx_log) # normalization component
 
-    return Sxx, Sxx_log
+    return Sxx, Sxx_log, samples
+
+
+def spectrogram_to_wav_griffinlim(spect, samples):
+
+    spect_mag = np.sqrt(spect)
+
+    reconstructed_signal = griffinlim(spect_mag, n_iter=32, hop_length=512, win_length=1024)
+    
+    reconstructed_signal = np.int16(reconstructed_signal / np.max(np.abs(reconstructed_signal)) * 32767)
+    
+    if os.path.exists("griffinlim_reconstruction.wav"): 
+        os.remove("griffinlim_reconstruction.wav")
+
+    write("griffinlim_reconstruction.wav", samples, reconstructed_signal)
